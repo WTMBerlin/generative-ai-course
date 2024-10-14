c45e4f93c38f125a929bd14486333ccf20f087e3
dashersw <armagan@amcalar.com>
2024-10-14 16:26:06 +0200

    Set port to 3002 to match the requests

--- week-1/js-project-3-function-calling-weather-bot/app.js
+++ week-1/js-project-3-function-calling-weather-bot/app.js
@@ -147,6 +147,6 @@ app.post('/ask', async (req, res) => {
   }
 })
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000')
+app.listen(3002, () => {
+  console.log('Server is running on port 3002')
 })
